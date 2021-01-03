ca70300aed89fefb74f766f94255e5821fcc554f
matto-Jay <mjohanni@student.wethinkcode.co.za>
2021-01-03 19:57:34 +0200

    added function to save client details to json folder

--- login_system/save_client_info.py
+++ login_system/save_client_info.py
@@ -4,7 +4,14 @@ def save_info(username,password):
     """Creates a json file and stores the user's information"""
 
     user_dict = {username : password}
-    file = json.dumps(user_dict)
-    f = open("accounts.json", "w")
-    f.write(file)
-    f.close()
+    with open("accounts.json", "r+") as file:
+        f = json.load(file)
+        f.update(user_dict)
+        file.seek(0)
+        json.dump(f,file)
+
+    # with open("accounts.json", "r+") as file:
+    # data = json.load(file)
+    # data.update(a_dictionary)
+    # file.seek(0)
+    # json.dump(data, file)
---
+++ login_system/test.py
@@ -0,0 +1,10 @@
+import json
+from save_client_info import save_info
+
+if __name__ == "__main__":
+    
+    save_info("matthew1","19940621mayheM.")
+    save_info("matthew11","19940621mayheM.")
+    save_info("matthew2","19940621mayheM.")
+    save_info("matthew3","19940621mayheM.")
+    save_info("jeff","123J123")
